bb9e72919cba3b22225b13eb3406462e8634ff6f
Skyler Mäntysaari <sm+git@skym.fi>
2026-04-22 00:06:25 +0300

    Initial Commit

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Skyler on 22.4.2026.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
